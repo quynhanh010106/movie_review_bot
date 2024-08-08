72d80aaa9b84c62bf7fdd7079b8c025fd5cae698
quynhanh010106 <star.nguyenquynhanh.0101@gmail.com>
2024-08-08 10:34:02 +0700

    imdb_id_to_title.py
    This is the code I used to change the input " tconst" from the original file imdb provided (which is an id for each film) to the output "title".
    The original information they provided was in <rating-short.xlsx> file (the one with no titile)

---
+++ imdb_id_to_title.py
@@ -0,0 +1,23 @@
+# Load the dataset from the uploaded file
+import pandas as pd
+file_path_notitle = '/content/rating-short.xlsx'
+imdb_data_notitle = pd.read_excel(file_path_notitle)
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_movie_name(imdb_id):
+  url = f"https://www.imdb.com/title/{imdb_id}/"
+  headers = {'User-Agent': 'Mozilla/5.0'}
+# Add a user-agent header
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.content, 'html.parser')
+  title = soup.find('h1').text.strip()
+  return title
+movie_names = []
+for imdb_id in imdb_data[' tconst']:
+  movie_names.append(get_movie_name(imdb_id))
+
+# Print the list of movie names
+print(movie_names)
+
